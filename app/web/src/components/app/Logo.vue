fb1ff949dde503a83abd93022f32cdc3c7e909ab
René Wagner <rene.papenfuss@getaway.group>
2026-04-17 09:27:00 +0200

    feat: update favicon to use ICO format, remove SVG, and add logo image

--- app/web/src/components/app/Logo.vue
+++ app/web/src/components/app/Logo.vue
@@ -1,28 +1,14 @@
 <script setup lang="ts">
 import { useAttrs } from "vue";
-import { storeToRefs } from "pinia";
-import { useHomepageStore } from "@/stores/homepageStore";
-import logoFallback from "@/assets/logo.svg";
 
 defineOptions({ inheritAttrs: false });
 
 const attrs = useAttrs();
-
-const homepageStore = useHomepageStore();
-const { homepageContent } = storeToRefs(homepageStore);
 </script>
 
 <template>
   <img
-    v-if="homepageContent?.heroLogo?.path"
-    :src="homepageContent.heroLogo.path"
-    alt="VSG Kugelberg Logo"
-    title="VSG Kugelberg Logo"
-    v-bind="attrs"
-  />
-  <img
-    v-else
-    :src="logoFallback"
+    src="/logo.png"
     alt="VSG Kugelberg Logo"
     title="VSG Kugelberg Logo"
     v-bind="attrs"
