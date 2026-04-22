42e1c357a90088974c9c31439cb8acf9eb71d91c
René Wagner <rene.papenfuss@getaway.group>
2026-04-22 15:26:31 +0200

    feat: import EasyMDE base CSS in main.ts

--- app/web/src/main.ts
+++ app/web/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./style.css";
+import "easymde/dist/easymde.min.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
